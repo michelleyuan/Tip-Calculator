e4bd0c2c4b5a59e1dcbe28c8ad3e0e3c8ef71857
Michelle Yuan <michelleyuan@wustl.edu>
2015-12-29 11:01:39 -0500

    initial commit with viewcontroller, settingsviewcontroller, and keyboard as first responder

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Tip Calculator"
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        self.tipControl.selectedSegmentIndex = defaults.integerForKey("default_tip_segment")
+        self.billField.becomeFirstResponder()
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        self.tipControl.selectedSegmentIndex = defaults.integerForKey("default_tip_segment")
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
